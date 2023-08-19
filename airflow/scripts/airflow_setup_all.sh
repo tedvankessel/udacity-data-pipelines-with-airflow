6aac82c099d2d368502fd0302f86c6ce8cd3679f
tedvankessel <tvk@us.ibm.com>
2023-08-19 13:26:18 -0400

    Update airflow_setup_all.sh

--- airflow/scripts/airflow_setup_all.sh
+++ airflow/scripts/airflow_setup_all.sh
@@ -1,6 +1,6 @@
 # add AWS Redshift connection
-airflow connections add redshift --conn-uri 'redshift://awsuser:Fat2q23cat@default-workgroup.595917921970.us-east-1.redshift-serverless.amazonaws.com:5439/dev'
+airflow connections add redshift --conn-uri 'redshift://awsuser:xxxxxxx@default-workgroup.595917921970.us-east-1.redshift-serverless.amazonaws.com:5439/dev'
 # add S3 bucket varaible
-airflow variables set s3_bucket tgvkbucket
+airflow variables set s3_bucket xxxxxxxxx
 # add aws credentials
-airflow connections add aws_credentials --conn-type 'aws' --conn-login 'AKIAYVP3Z32ZLAWBPSRW' --conn-password 'Ma2LWc/ifge5GElvZ6aRcuq8HJgeiQ4LXgD0GpLL'
+airflow connections add aws_credentials --conn-type 'aws' --conn-login 'xxxxxxxxx' --conn-password 'xxxxxxxxxxx'
